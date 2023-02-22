8d5e9f57ea3f26900a5b714686ae608c73a15804
Tatiana2424 <tanja3708@gmail.com>
2023-02-22 21:03:14 +0200

    Added entity

---
+++ BarberShop/BarberShop.DAL/Entities/Image.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations; 
+
+namespace BarberShop.DAL.Entities;
+
+[Table("images", Schema = "media")]
+public class Image
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string? Title { get; set; }
+
+    [MaxLength(100)]
+    public string? Alt { get; set; }
+
+    [Required]
+    public string Url { get; set; }
+
+    public List<Category> CategoryImages { get; set; }
+
+    public List<Service> ServiceImages { get; set; }
+
+    public List<Barber> BarberImages { get; set; }
+
+}
---
+++ BarberShop/BarberShop.DAL/Entities/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberShop.DAL.Entities;
+
+public class Order
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public Barber Barber { get; set; }
+
+    [Required]
+    public int BarberId { get; set; }
+
+    public User User { get; set; }
+
+    public int UserId { get; set; }
+
+    public Service Service { get; set; }
+ 
+    public int ServiceId { get; set; }
+
+    public Place Place { get; set; }
+
+    public int PlaceId { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime date { get; set; }
+
+    [Required]
+    public TimeSpan time { get; set; }
+}
